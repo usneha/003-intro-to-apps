9bc47f5edd0ef8b2c20d3f30f1231f9b1ba63b07
usneha <usneha@hawk.iit.edu>
2022-07-12 22:29:26 +0000

    changed colors

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly.graph_objs as go
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
+color1='yellow'
+color2='blue'
 mytitle='Beer Comparison'
 
 label1='IBU'
